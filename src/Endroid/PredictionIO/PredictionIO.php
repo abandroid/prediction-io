ca2874a9bdbb18afe9c348a83f14fe9cd324f05d
endroid <info@endroid.nl>
2013-10-21 20:44:13 -0400

    Implement client functions

--- src/Endroid/PredictionIO/PredictionIO.php
+++ src/Endroid/PredictionIO/PredictionIO.php
@@ -41,4 +41,82 @@ class PredictionIO
 
         $this->client = PredictionIOClient::factory($config);
     }
+
+    /**
+     * Create a user.
+     *
+     * @param $userId
+     * @return mixed
+     */
+    public function createUser($userId)
+    {
+        $command = $this->client->getCommand('create_user', array('pio_uid' => $userId));
+        $response = $this->client->execute($command);
+
+        return $response;
+    }
+
+    /**
+     * Create an item.
+     *
+     * @param $itemId
+     * @param int $itemTypes
+     * @return mixed
+     */
+    public function createItem($itemId, $itemTypes = 1)
+    {
+        $command = $this->client->getCommand('create_item', array('pio_iid' => $itemId, 'pio_itypes' => $itemTypes));
+        $response = $this->client->execute($command);
+
+        return $response;
+    }
+
+    /**
+     * Record a user action on an item.
+     *
+     * @param $userId
+     * @param $itemId
+     * @param string $action
+     * @return mixed
+     */
+    public function recordAction($userId, $itemId, $action = 'view')
+    {
+        $this->client->identify($userId);
+        $response = $this->client->execute($this->client->getCommand('record_action_on_item', array('pio_action' => $action, 'pio_iid' => $itemId)));
+
+        return $response;
+    }
+
+    /**
+     * Returns the recommendations for the given user according to the engine.
+     *
+     * @param $userId
+     * @param $engine
+     * @param int $count
+     * @return mixed
+     */
+    public function getRecommendations($userId, $engine, $count = 3)
+    {
+        $this->client->identify($userId);
+        $command = $this->client->getCommand('itemrec_get_top_n', array('pio_engine' => $engine, 'pio_n' => $count));
+        $response = $this->client->execute($command);
+
+        return $response;
+    }
+
+    /**
+     * Returns the items similar to the given item according to the engine.
+     *
+     * @param $itemId
+     * @param $engine
+     * @param int $count
+     * @return mixed
+     */
+    public function getSimilarItems($itemId, $engine, $count = 3)
+    {
+        $command = $this->client->getCommand('itemsim_get_top_n', array('pio_iid' => $itemId, 'pio_engine' => $engine, 'pio_n' => $count));
+        $response = $this->client->execute($command);
+
+        return $response;
+    }
 }
