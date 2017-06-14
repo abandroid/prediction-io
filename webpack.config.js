a5ddd55883ab5aa4cd4948fe2bdbb3273b7e37c3
endroid <info@endroid.nl>
2017-06-14 21:14:51 +0000

    Simplify configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,13 @@
-var webpack = require('webpack');
+let Encore = require('@symfony/webpack-encore');
 
-var production = process.env.NODE_ENV === 'production';
+Encore
+    .setOutputPath('src/Bundle/Resources/public/build/')
+    .setPublicPath('/build')
+    .cleanupOutputBeforeBuild()
+    .addEntry('index', './src/Bundle/Resources/public/src/js/index.js')
+    .autoProvidejQuery()
+    .enableReactPreset()
+    .enableSourceMaps(!Encore.isProduction())
+;
 
-module.exports = {
-    entry: {
-        index: './src/Bundle/Resources/public/src/js/index.js'
-    },
-    output: {
-        path: './src/Bundle/Resources/public/builds',
-        filename: '[name].js',
-        publicPath: production ? '/endroidpredictionio/builds/' : 'http://localhost:8080/builds/'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-            {
-                test: /\.css$/,
-                loader: "style!css"
-            },
-            {
-                test:   /\.((eot|ttf|woff|woff2|png|gif|jpe?g|svg)?(\?v=[0-9]\.[0-9]\.[0-9])?)$/i,
-                loader: 'url?limit=10000',
-            }
-        ]
-    },
-    devServer: {
-        hot: true,
-        contentBase: './src/Bundle/Resources/public/src/',
-        headers: { "Access-Control-Allow-Origin": "*" }
-    },
-    plugins: production ? [
-        new webpack.DefinePlugin({
-            'process.env': { NODE_ENV: JSON.stringify("production") }
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false }
-        })
-    ] : [],
-    devtool: production ? 'cheap-module-source-map' : 'cheap-module-eval-source-map'
-};
+module.exports = Encore.getWebpackConfig();
