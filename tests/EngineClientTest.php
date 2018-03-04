b006185472868b50b098e2909dbb7e5a31e79359
endroid <info@endroid.nl>
2018-03-04 21:53:45 +0000

    Create basic test

---
+++ tests/EngineClientTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * (c) Jeroen van den Enden <info@endroid.nl>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Endroid\Guide\Tests;
+
+use Endroid\PredictionIo\EngineClient;
+use GuzzleHttp\Exception\ConnectException;
+use PHPUnit\Framework\TestCase;
+
+class EngineClientTest extends TestCase
+{
+    public function testNoTestsYet()
+    {
+        $engineClient = new EngineClient('http://localhost:8000');
+
+        $this->expectException(ConnectException::class);
+        $engineClient->getRecommendedItems('user_1', 3);
+    }
+}
